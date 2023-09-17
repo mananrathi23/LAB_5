2862db534d1849dc2c509cba8a555d576a94b8cf
mananrathi23 <144107804+mananrathi23@users.noreply.github.com>
2023-09-17 16:18:45 +0530

    Create Q3.py

---
+++ Q3.py
@@ -0,0 +1,16 @@
+i= 1 
+n= int(input("Enter number of lines you want to print: ")) 
+while i<=n: 
+    print("* "*i) 
+    i= i+1 
+  
+  
+ #b) 
+  
+i= 1 
+n= int(input("Enter number of lines you want to print: ")) 
+a=(2*n)-1 
+while i<=n: 
+    print(" "*a,"* "*i) 
+    i= i+1 
+    a-=2
