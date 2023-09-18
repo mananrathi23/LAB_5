5475b38922a8eabf4d3050ded3aaeb1060ee6490
mananrathi23 <144107804+mananrathi23@users.noreply.github.com>
2023-09-18 19:10:19 +0530

    create Q6.py

--- Q6.py
+++ Q6.py
@@ -1,10 +1,21 @@
-num1 = int(input("enter the number:))
-num2 = int(input("enter the number:))
-a =num1
-b =num2
-while num1 != num2:
-  if num1 > num2:
-    num1 -= num2
-  else:
-    num2 -= num1
-print("hcf of ",a," and ",b," is ",num1)
+count = int(input("How many numbers you want to take HCF: "))
+while count <= 0 :
+    print("Invalid input")
+    count = int(input("How many numbers you want to take HCF: "))
+num=int(input("enter number 1:"))    
+while num<=0:    
+    print("Invalid input")
+    n=int(input("enter number 1:"))
+
+i = 1
+while i < count:
+    n = int(input(f"Enter number {i+1}: "))
+    if n <=0:
+        print("Invalid input")
+        n = int(input(f"Enter number {i+1}: "))
+    else:
+        while n:
+            temp=num
+            num=n
+            n=temp%n  
+        i += 1
